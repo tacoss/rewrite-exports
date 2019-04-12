961d2c9d079dc2b46149c4431a4bc1071848db84
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-04-11 23:19:54 -0500

    First impl ready

---
+++ index.js
@@ -0,0 +1,61 @@
+const RE_KEYWORD = /(\bdefault\s+)?\b(let|const|class|function)\s+((\w+)(.*))$/i;
+const RE_EXPORT = /^(\s*)export\s+([^\n;]*)/gmi;
+const RE_FROM = /\bfrom\s+(["'])([^"']*)\1/gi;
+const RE_DF = /\bdefault(\s+as\s+(\w+))?\b/i;
+const RE_AS = /\b(\w+)\s+as\s+(\w+)\b/gi;
+const RE_EQ = /\s*=\s*/;
+
+function replaceExport(_, left, tokens) {
+  let prefix = `${left}module.exports`;
+
+  const symbols = tokens.match(RE_KEYWORD);
+
+  if (symbols) {
+    if (symbols[2] === 'let' || symbols[2] === 'const') {
+      return `${left}${tokens}; module.exports = { ${symbols[3].split(RE_EQ).join(', ')} }`;
+    }
+
+    if (!symbols[1]) {
+      prefix += `.${symbols[4]}`;
+    }
+  }
+
+  const def = tokens.match(RE_DF);
+
+  if (tokens.match(RE_FROM)) {
+    const vars = tokens.replace(RE_AS, '$2').replace(RE_FROM, '').trim();
+
+    tokens = tokens.replace(RE_FROM, '= require("$2")');
+    tokens = tokens.replace(RE_AS, '$1: $2');
+
+    const req = tokens.split('=').pop().trim();
+
+    if (vars === '*') {
+      return `${prefix} = ${req}`;
+    }
+
+    if (def) {
+      if (def[2]) {
+        prefix += `.${def[2]}`;
+      }
+
+      return `${prefix} = ${req}`;
+    }
+
+    return `${left}const ${tokens}; module.exports = ${vars}`;
+  }
+
+  if (def) {
+    if (symbols || !tokens.match(RE_AS)) {
+      tokens = tokens.replace(RE_DF, '').trim();
+    } else {
+      tokens = tokens.match(RE_AS)[0].split(' ').shift();
+    }
+  } else {
+    tokens = tokens.replace(RE_AS, '$2: $1');
+  }
+
+  return `${prefix} = ${tokens}`;
+}
+
+module.exports = code => code.replace(RE_EXPORT, replaceExport);
